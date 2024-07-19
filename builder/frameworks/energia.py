a72690c79c7dfed0cfe3f5dac3d82a6e357eecdd
曾钦李 <1838696034@qq.com>
2024-07-19 14:55:14 +0800

    finish

---
+++ builder/frameworks/energia.py
@@ -0,0 +1,47 @@
+"""
+Based on https://github.com/energia/msp432r-core
+References https://energia.nu/reference/
+"""
+
+from os.path import isdir, join
+
+from SCons.Script import DefaultEnvironment
+
+env = DefaultEnvironment()
+platform = env.PioPlatform()
+
+FRAMEWORK_DIR = platform.get_package_dir("framework-energiamsp432e")
+FRAMEWORK_VERSION = platform.get_package_version("framework-energiamsp432e")
+assert isdir(FRAMEWORK_DIR)
+
+board = env.BoardConfig()
+
+variants_dir = join(
+    "$PROJECT_DIR", board.get("build.variants_dir")) if board.get(
+        "build.variants_dir", "") else join(FRAMEWORK_DIR, "variants")
+
+core_lib = env.BuildLibrary(
+    join("$BUILD_DIR", "core_%s" % board.get("build.core")),
+    join(FRAMEWORK_DIR, 'cores', board.get("build.core"), 'ti/runtime/wiring'),
+),
+
+env.Append(
+    CPPDEFINES=[
+        ("ARDUINO", 10807),
+        ("ENERGIA", int(FRAMEWORK_VERSION.split(".")[1])),
+    ],
+    _LIBDIRFLAGS=[core_lib],
+    LIBS=[core_lib],
+)
+
+env.BuildSources(
+    join("$BUILD_DIR", "arduino_%s" % (board.get("build.core"))),
+    join(FRAMEWORK_DIR, 'extras/arduino'),
+)
+
+env.BuildSources(
+    join("$BUILD_DIR", "core_%s_%s" % (board.get("build.core"), board.get("build.variant"))),
+    join(FRAMEWORK_DIR, 'variants', board.get("build.variant")),
+)
+
+env.SConscript("tirtos.py")
---
+++ examples/native-blink/README.md
@@ -0,0 +1,20 @@
+How to build PlatformIO based project
+=====================================
+
+1. [Install PlatformIO Core](http://docs.platformio.org/page/core.html)
+2. Clone this example code
+3. Run these commands:
+
+```shell
+# Change directory to example
+$ cd platform-timsp432e/examples/native-blink
+
+# Build project
+$ pio run
+
+# Upload firmware
+$ pio run --target upload
+
+# Clean build files
+$ pio run --target clean
+```
